13cefd5ad48d0e51bf7693179ebb33aff162ac12
Giovanna Russo <russopgiovanna@gmail.com>
2013-02-19 22:36:22 -0430

    Initial commit

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+define("DB_DSN", "mysql:dbname=book_club" );
+define("DB_USERNAME", "root" );
+define("DB_PASSWORD", "1234" );
+define("PAGE_SIZE" , 5 );
+define("TBL_MEMBERS", "members" );
+define("TBL_ACCESS_LOG", "accessLog" );
+?>
+
+
+
+
+
+
+
+
